a677ee1918cfa6fad3970e11af6d73aecdf19fed
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-17 09:45:20 +0100

    Create colors.ts

---
+++ src/constants/colors.ts
@@ -0,0 +1,91 @@
+import { rgba } from 'polished';
+
+const bitterSweet = 'rgb(252,101,82)' as const;
+const bluewood = 'rgb(50,73,86)' as const;
+const slate = 'rgb(109,127,139)' as const;
+const cadetBlue = 'rgb(167,181,191)' as const;
+const candyPink = 'rgb(237,97,98)' as const;
+const carolinaBlue = 'rgb(70,156,221)' as const;
+const mauvelous = 'rgb(226, 152, 170)' as const;
+const copper = 'rgb(184,111,86)' as const;
+const darkTangaroa = 'rgb(24,43,53)' as const;
+const emerald = 'rgb(79,194,123)' as const;
+const firefly = 'rgb(18,34,44)' as const;
+const frostbite = 'rgb(221,72,180)' as const;
+const iceBlue = 'rgb(233,246,255)' as const;
+const malachite = 'rgb(90,226,76)' as const;
+const mango = 'rgb(236,126,60)' as const;
+const mindaro = 'rgb(206, 254, 136)' as const;
+const mint = 'rgb(163,255,159)' as const;
+const paleSkyBlue = 'rgb(209,222,231)' as const;
+const rose = 'rgb(234,72,115)' as const;
+const shamrock = 'rgb(72,221,180)' as const;
+const silk = 'rgb(255,235,204)' as const;
+const straw = 'rgb(221,216,72)' as const;
+const tangaroa = 'rgb(28,49,62)' as const;
+const turquoise = 'rgb(79,197,199)' as const;
+const violetBlue = 'rgb(123,114,198)' as const;
+const xanthic = 'rgb(230,230,28)' as const;
+
+export const white0 = 'rgba(255,255,255,.0)' as const;
+export const white05 = 'rgba(255,255,255,.05)' as const;
+export const white10 = 'rgba(255,255,255,.1)' as const;
+export const white15 = 'rgba(255,255,255,.15)' as const;
+export const white30 = 'rgba(255,255,255,.3)' as const;
+export const white50 = 'rgba(255,255,255,.5)' as const;
+export const white80 = 'rgba(255,255,255,.8)' as const;
+export const white = 'rgba(255,255,255,1)' as const;
+
+export const black0 = 'rgba(0,0,0,0)' as const;
+export const black05 = 'rgba(0,0,0,.05)' as const;
+export const black10 = 'rgba(0,0,0,.1)' as const;
+export const black15 = 'rgba(0,0,0,.15)' as const;
+export const black25 = 'rgba(0,0,0,.25)' as const;
+export const black40 = 'rgba(0,0,0,.4)' as const;
+export const black50 = 'rgba(0,0,0,.5)' as const;
+export const black80 = 'rgba(0,0,0,.8)' as const;
+export const black = 'rgba(0,0,0,1)' as const;
+
+export const gray = 'rgb(216,216,216)' as const;
+export const gray50 = 'rgba(187,187,187,0.5)' as const;
+export const subtleGray = 'rgb(245,245,245)' as const;
+
+export const primary = tangaroa;
+export const primary100 = firefly;
+export const primary200 = darkTangaroa;
+export const primary300 = tangaroa;
+export const primary400 = bluewood;
+export const primary500 = slate;
+export const primary600 = cadetBlue;
+export const primary700 = paleSkyBlue;
+export const primary800 = iceBlue;
+
+export const secondary = shamrock;
+export const secondary20 = rgba(shamrock, 0.2);
+export const secondary30 = rgba(shamrock, 0.3);
+export const secondary40 = rgba(shamrock, 0.4);
+export const secondary50 = rgba(shamrock, 0.5);
+export const secondary60 = rgba(shamrock, 0.6);
+export const secondary70 = rgba(shamrock, 0.7);
+export const secondary80 = rgba(shamrock, 0.8);
+
+export const utilityBlue = carolinaBlue;
+export const utilityGreen = malachite;
+export const utilityOrange = mango;
+export const utilityPink = frostbite;
+export const utilityRed = candyPink;
+export const utilityYellow = straw;
+export const utilityLightGreen = mint;
+
+export const specialBeige = silk;
+export const specialBlue = turquoise;
+export const specialBrown = copper;
+export const specialLightGreen = mindaro;
+export const specialGreen = emerald;
+export const specialPink = rose;
+export const specialPurple = violetBlue;
+export const specialRed = bitterSweet;
+export const specialLightRed = mauvelous;
+export const specialYellow = xanthic;
+
+export const highlightYellow = rgba(straw, 0.5);
