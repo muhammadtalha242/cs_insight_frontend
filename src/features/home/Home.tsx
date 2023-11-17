cfa2059ee8eb17129a79be1b730171271b276c87
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-17 09:46:03 +0100

    Update Home.tsx

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Header } from "../../components/Header";
 
 export const Home: React.FC = () => {
-  return <> TEST</>;
+  return (
+    <>
+      <Header />
+      TEST
+    </>
+  );
 };
