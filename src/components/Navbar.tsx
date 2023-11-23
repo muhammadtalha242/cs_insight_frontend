f2ac459d4d8d7d59b31db69d5b5cb8a4ea237eaa
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-23 01:16:11 +0100

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+import styled from "styled-components";
+
+const narBarMenuItems: MenuProps["items"] = [
+  { key: "Home", label: <>Home</>, title: "Home" },
+  {
+    key: "Search",
+    label: <>Search</>,
+    title: "Search",
+    children: [
+      { key: "Papers", label: <>Paper</> },
+      { key: "Authors", label: <>Authors</> },
+      { key: "Venues", label: <>Venues</> },
+    ],
+  },
+];
+const NavbarMenuContainer = styled(Menu)`
+  width: -webkit-fill-available;
+`;
+export const Navbar: React.FC = () => {
+  return (
+    <NavbarMenuContainer
+      mode="horizontal"
+      defaultSelectedKeys={["Home"]}
+      items={narBarMenuItems}
+    />
+  );
+};
