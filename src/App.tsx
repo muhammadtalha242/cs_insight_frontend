2c613bb3b1c6ad54448981ba45599ff37d3bd129
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-23 01:16:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import { ConfigProvider } from "antd";
 import React from "react";
+import { ConfigProvider, Layout } from "antd";
 
 import "./App.css";
 import Home from "./features/home";
+import { Header } from "./components/Header";
 
 const App: React.FC = () => (
   <ConfigProvider
     theme={{
       token: {
         // Seed Token
-        colorPrimary: "#00b96b",
-        borderRadius: 2,
-
-        // Alias Token
-        colorBgContainer: "#f6ffed",
+        // colorPrimary: "#00b96b",
+        // borderRadius: 2,
+        // // Alias Token
+        // colorBgContainer: "#f6ffed",
       },
     }}
   >
-    <Home />
+    <Layout>
+      <Header />
+
+      <Home />
+    </Layout>
   </ConfigProvider>
 );
 
