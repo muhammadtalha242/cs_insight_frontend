2c8d9b802e2c59604bfe910eac2af9e1bf806a00
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-27 04:47:01 +0100

    Create Drawer.tsx

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Drawer as DrawerAntd } from "antd";
+import styled from "styled-components";
+
+const DrawerContainer = styled(DrawerAntd)``;
+
+const Drawer: React.FC<{
+  isOpen: boolean;
+  onClose: () => void;
+}> = ({ isOpen, onClose }) => {
+  return (
+    <DrawerContainer
+      title="Filter"
+      placement="right"
+      closable={true}
+      onClose={onClose}
+      open={isOpen}
+      getContainer={false}
+    >
+      <p>Some contents...</p>
+    </DrawerContainer>
+  );
+};
+
+export default Drawer;
