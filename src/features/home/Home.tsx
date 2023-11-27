c88e22d32330322bd420ef2da38341ba1a2c9af7
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-27 05:54:45 +0100

    Update Home.tsx

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Layout } from "antd";
+import React, { useCallback, useState } from "react";
+import { Button, Layout } from "antd";
 import CombinedInput from "../../components/CombinedInput";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
@@ -17,6 +17,12 @@ const ContentStyledContainer = styled(Content)`
   width: 100%;
 `;
 
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 //TODO: Create constants: Papers, Authors and the string mapping around the application
 export type StateProps = {
   dataSet: string;
@@ -25,21 +31,43 @@ export type StateProps = {
 
 export const Home: React.FC = () => {
   const [values, setValues] = useState<StateProps>({
-    dataSet: "Authors",
+    dataSet: "authors",
     query: "5G",
   });
   const navigate = useNavigate();
 
-  const onSubmit = (updatedValues: StateProps) => {
+  const onSubmit = useCallback((updatedValues: StateProps) => {
     setValues((prev) => ({ ...prev, ...updatedValues }));
     console.log("{ ...prev, ...updatedValues }", { ...updatedValues });
-    navigate(`/search?dataSet=${values.dataSet}&query=${values.query}`);
-  };
+    navigate(`/search/${updatedValues.dataSet}?query=${updatedValues.query}`);
+  }, []);
+
+  const handleChangeView = useCallback((dataSet: string) => {
+    onSubmit({ dataSet, query: "all" });
+  }, []);
 
   return (
     <ContentStyledContainer>
       HOME
       <CombinedInput initialValues={values} onSubmit={onSubmit} />
+      <ButtonGroup>
+        <Button
+          id="papers"
+          name="papers"
+          onClick={() => handleChangeView("papers")}
+        >
+          Papers
+        </Button>
+        <Button
+          id="authors"
+          name="authors"
+          onClick={() => handleChangeView("authors")}
+        >
+          Authors
+        </Button>
+        <Button disabled>Venue</Button>
+        <Button disabled>Topics</Button>
+      </ButtonGroup>
     </ContentStyledContainer>
   );
 };
