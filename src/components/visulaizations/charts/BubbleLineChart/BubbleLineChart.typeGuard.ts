4cb78eddf51eb1409de13525fdfd176dacf20983
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-30 17:46:39 +0100

    Create BubbleLineChart.typeGuard.ts

---
+++ src/components/visulaizations/charts/BubbleLineChart/BubbleLineChart.typeGuard.ts
@@ -0,0 +1,9 @@
+import { LineData, Point } from './BubbleLineChart.types';
+
+export function isPoint(d: Point | LineData): d is Point {
+  return !('points' in d);
+}
+
+export function isLine(d: Point | LineData): d is LineData {
+  return 'points' in d;
+}
