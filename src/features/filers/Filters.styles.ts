93926b160d199c370563e792771bf36e2ad31b96
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-30 17:47:10 +0100

    Create Filters.styles.ts

---
+++ src/features/filers/Filters.styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const FilterContentContainer = styled.div`
+    padding: 16px 24px;
+`
+
+export const InputNumberFilterContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`
+export const SelectFilterContainer = styled.div`
+`
+
+
+export const LabelFilterContainer = styled.label`
+    text-align: start;
+    margin-bottom: 8px;
+    display: flex;
+    justify-content: space-between;
+    font-size: medium;
+    font-weight: 600;
+`
