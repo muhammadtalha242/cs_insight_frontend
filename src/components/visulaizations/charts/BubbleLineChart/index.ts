9d8a42211d1419f17ab37954542a723c14b36a21
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-30 17:46:43 +0100

    Create index.ts

---
+++ src/components/visulaizations/charts/BubbleLineChart/index.ts
@@ -0,0 +1,5 @@
+import { BubbleLineChart  } from './BubbleLineChart';
+
+export type { BubbleLineChartProps }from './BubbleLineChart';
+export type { LineData, Point } from './BubbleLineChart.types';
+export default BubbleLineChart  ;
