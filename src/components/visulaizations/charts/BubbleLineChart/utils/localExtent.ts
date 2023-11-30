3ef394364723255309c54fb00d045cba7984b538
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-30 17:46:48 +0100

    Create localExtent.ts

---
+++ src/components/visulaizations/charts/BubbleLineChart/utils/localExtent.ts
@@ -0,0 +1,33 @@
+import type { Dimensions, Point, Range } from '../BubbleLineChart.types';
+
+export function localExtent(
+  points: Point[],
+  target: Dimensions,
+  base: Dimensions,
+  baseDomain: Range
+): Range {
+  if (baseDomain[0] === undefined || baseDomain[1] === undefined) {
+    return [undefined, undefined];
+  }
+
+  let extent: Range = [undefined, undefined];
+
+  for (const p of points) {
+    const b = p[base];
+    const v = p[target];
+
+    if (b !== undefined && v !== undefined) {
+      if (baseDomain[0] <= b && b <= baseDomain[1]) {
+        if (extent[0] === undefined || extent[1] === undefined) {
+          extent = [v, v];
+        } else if (extent[1] < v) {
+          extent[1] = v;
+        } else if (v < extent[0]) {
+          extent[0] = v;
+        }
+      }
+    }
+  }
+
+  return extent;
+}
