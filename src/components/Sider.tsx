32351f6041cce0e0401820caac4514d8ee12e9d8
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-11-30 17:46:21 +0100

    Create Sider.tsx

---
+++ src/components/Sider.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+const SiderContainer = styled.aside`
+  position: fixed;
+  right: 0;
+  background-color: white;
+  border-left: 1px solid #ddd;
+  transition: width 0.3s ease;
+  min-width: 250px;
+
+  &.collapsed {
+    width: 0;
+    min-width: 0;
+    max-width: 0;
+    overflow: hidden;
+    transition: width 0.3s ease;
+  }
+`;
+
+export type SiderProps = {
+  collapsed: boolean;
+  children?: ReactNode;
+};
+const Sider: React.FC<SiderProps> = ({ children, collapsed }) => {
+  return (
+    <SiderContainer className={!collapsed ? "collapsed" : ""}>
+      {children}
+    </SiderContainer>
+  );
+};
+
+export default Sider;
