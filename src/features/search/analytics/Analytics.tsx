d05cf90a858230b1109fc12050be61972ef54948
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-13 16:53:08 +0100

    Update Analytics.tsx

--- src/features/search/analytics/Analytics.tsx
+++ src/features/search/analytics/Analytics.tsx
@@ -15,31 +15,6 @@ export const Analytics: React.FC = () => {
   };
 
   const data = papersCountPerYear;
-  // const [active, setActive] = useState<Point | LineData | undefined>();
-
-  // // const points = useMemo(
-  // //   () =>
-  // //     data.reduce((acc, current) => {
-  // //       acc.push(...current.points);
-
-  // //       return acc;
-  // //     }, [] as Point[]),
-  // //   [data]
-  // // );
-
-  // const extentX = useMemo(
-  //   () => d3.extent(data, (d) => d.x) as [number, number],
-  //   [data]
-  //   );
-  //   const [domainX, setDomainX1] = useState(() => extentX);
-  //   <BubbleLineChart
-  //     active={active}
-  //     colorMap={"rgb(70,156,221)"}
-  //     data={data}
-  //     domainX={domainX}
-  //     interpolateX={false}
-  //     axisFormatX="integer-without-grouping"
-  //   />
 
   const visualizationsAll: TabsProps["items"] = [
     {
@@ -59,35 +34,7 @@ export const Analytics: React.FC = () => {
     },
   ];
 
-  const visualizationsSelected: TabsProps["items"] = [
-    {
-      key: "line-chart",
-      label: "Distribution of Papers",
-      children:
-        "Visualize the distribution of papers across different categories such as years, venues, or topics. Show trends in the number of papers over time.",
-    },
-    {
-      key: "2",
-      label: "Tab 2",
-      children: "Content of Tab Pane 2",
-    },
-    {
-      key: "3",
-      label: "Tab 3",
-      children: "Content of Tab Pane 3",
-    },
-  ];
-
-  // const papersPerYear = _.groupBy(papers, "year");
-  // console.log("papersPerYear", papersPerYear);
-  // const papersCountPerYear = Object.entries(papersPerYear).map(
-  //   ([key, value]) => ({
-  //     x1: key,
-  //     x2: value.length,
-  //     x3: 1,
-  //   })
-  // );
-  // console.log("papersCountPerYear", papersCountPerYear);
+  const visualizationsSelected: TabsProps["items"] = [];
 
   return (
     <>
