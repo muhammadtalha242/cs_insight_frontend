7923185256fee3d70f6db1f0c0a9659f9945330d
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-13 16:53:15 +0100

    Update DistributionOverTime.tsx

--- src/features/visualizations/distributionOverTime/DistributionOverTime.tsx
+++ src/features/visualizations/distributionOverTime/DistributionOverTime.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import BubbleLineChart, {
   BubbleLineChartProps,
 } from "../../../components/visulaizations/charts/BubbleLineChart";
+import useComponentSize from "@rehooks/component-size";
 
-const DistributionOverTime: React.FC<BubbleLineChartProps> = ({ data }) => {
-  return <BubbleLineChart data={data} />;
+const DistributionOverTime: React.FC<Omit<BubbleLineChartProps, "ref">> = ({
+  data,
+}) => {
+  const chartRef = useRef<null>(null);
+
+  let { width, height } = useComponentSize(chartRef);
+  console.log("width, height ", width, height);
+
+  return (
+    <div style={{ outline: "1px solid" }} ref={chartRef}>
+      <BubbleLineChart data={data} />
+    </div>
+  );
 };
 
 export default DistributionOverTime;
