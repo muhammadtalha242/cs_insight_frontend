253aa82cdc473ad68ea2ccdfbd4265f792efb1bd
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-13 16:52:55 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import React, { ReactNode } from "react";
 import styled from "styled-components";
 import { Layout } from "antd";
 
-import { black80 } from "../constants/colors";
+import { Color } from "../themes";
 
 const { Header: HeaderAntd } = Layout;
 
 const HeaderComponets = styled(HeaderAntd)`
-  border-bottom: 1px ${black80} solid;
+  border-bottom: 1px ${Color.black80} solid;
   display: flex;
   justify-content: space-between;
   align-items: center;
