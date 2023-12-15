9fd88a264f82e2935bf112c70244db311a42852c
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-15 18:27:55 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,16 @@ const HeaderComponets = styled(HeaderAntd)`
   align-items: center;
   padding-right: 20px;
 `;
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+};
 
 const Header: React.FC<{ children?: ReactNode }> = ({ children }) => {
-  return <HeaderComponets>{children}</HeaderComponets>;
+  return <HeaderComponets style={headerStyle}>{children}</HeaderComponets>;
 };
 
 export default Header;
