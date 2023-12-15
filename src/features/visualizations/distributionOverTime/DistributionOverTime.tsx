573cec08e9773c2fa3dfd9c5ef1be677b514e7a9
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-15 18:28:09 +0100

    Update DistributionOverTime.tsx

--- src/features/visualizations/distributionOverTime/DistributionOverTime.tsx
+++ src/features/visualizations/distributionOverTime/DistributionOverTime.tsx
@@ -1,20 +1,41 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import BubbleLineChart, {
   BubbleLineChartProps,
 } from "../../../components/visulaizations/charts/BubbleLineChart";
 import useComponentSize from "@rehooks/component-size";
 
-const DistributionOverTime: React.FC<Omit<BubbleLineChartProps, "ref">> = ({
-  data,
-}) => {
-  const chartRef = useRef<null>(null);
+const DistributionOverTime: React.FC<
+  Omit<BubbleLineChartProps, "dimensions">
+> = ({ data }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  // let { width, height } = useComponentSize(containerRef);
+  const [size, setSize] = useState({ width: 0, height: 0 });
 
-  let { width, height } = useComponentSize(chartRef);
-  console.log("width, height ", width, height);
+  useEffect(() => {
+    const handleResize = () => {
+      if (containerRef.current) {
+        setSize({
+          width: containerRef.current.offsetWidth,
+          height: containerRef.current.offsetHeight,
+        });
+      }
+    };
 
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial size update
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <div style={{ outline: "1px solid" }} ref={chartRef}>
-      <BubbleLineChart data={data} />
+    <div
+      ref={containerRef}
+      style={{ width: "100%", height: "100%", backgroundColor: "lightblue" }}
+    >
+      <BubbleLineChart
+        data={data}
+        dimensions={{ width: size.width, height: size.height }}
+        // dimensions={{ width, height }}
+      />
     </div>
   );
 };
