6bf74b16377c04fcc51301ab9e6149b4865dee83
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-16 21:40:39 +0100

    Update Analytics.tsx

--- src/features/search/analytics/Analytics.tsx
+++ src/features/search/analytics/Analytics.tsx
@@ -8,31 +8,29 @@ import papersCountPerYear from "../../../../public/paper-count-per-year.json";
 import DistributionOverTime from "../../visualizations/distributionOverTime/DistributionOverTime";
 
 export type ColorMap = Record<string, string>;
+const data = papersCountPerYear;
 
+export const visualizationsAll: TabsProps["items"] = [
+  {
+    key: "line-chart",
+    label: "Distribution of Papers",
+    children: <DistributionOverTime data={data} />,
+  },
+  {
+    key: "2",
+    label: "Tab 2",
+    children: "Content of Tab Pane 2",
+  },
+  {
+    key: "3",
+    label: "Tab 3",
+    children: "Content of Tab Pane 3",
+  },
+];
 export const Analytics: React.FC = () => {
   const onChange = (key: string) => {
     console.log(key);
   };
-
-  const data = papersCountPerYear;
-
-  const visualizationsAll: TabsProps["items"] = [
-    {
-      key: "line-chart",
-      label: "Distribution of Papers",
-      children: <DistributionOverTime data={data} />,
-    },
-    {
-      key: "2",
-      label: "Tab 2",
-      children: "Content of Tab Pane 2",
-    },
-    {
-      key: "3",
-      label: "Tab 3",
-      children: "Content of Tab Pane 3",
-    },
-  ];
 
   const visualizationsSelected: TabsProps["items"] = [];
 
