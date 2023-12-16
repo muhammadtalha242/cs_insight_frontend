a00452db27973eaf29c1bc2b800c1db5f9349229
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-16 21:40:58 +0100

    Update DistributionOverTime.tsx

--- src/features/visualizations/distributionOverTime/DistributionOverTime.tsx
+++ src/features/visualizations/distributionOverTime/DistributionOverTime.tsx
@@ -1,43 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import BubbleLineChart, {
   BubbleLineChartProps,
 } from "../../../components/visulaizations/charts/BubbleLineChart";
-import useComponentSize from "@rehooks/component-size";
 
 const DistributionOverTime: React.FC<
   Omit<BubbleLineChartProps, "dimensions">
 > = ({ data }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  // let { width, height } = useComponentSize(containerRef);
-  const [size, setSize] = useState({ width: 0, height: 0 });
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (containerRef.current) {
-        setSize({
-          width: containerRef.current.offsetWidth,
-          height: containerRef.current.offsetHeight,
-        });
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Initial size update
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  return (
-    <div
-      ref={containerRef}
-      style={{ width: "100%", height: "100%", backgroundColor: "lightblue" }}
-    >
-      <BubbleLineChart
-        data={data}
-        dimensions={{ width: size.width, height: size.height }}
-        // dimensions={{ width, height }}
-      />
-    </div>
-  );
+  return <BubbleLineChart data={data} />;
 };
 
 export default DistributionOverTime;
