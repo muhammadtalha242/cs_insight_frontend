fd633fa5c650fecce7e30324e09df05dd6d9cf16
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-18 00:49:39 +0100

    Update Analytics.tsx

--- src/features/search/analytics/Analytics.tsx
+++ src/features/search/analytics/Analytics.tsx
@@ -1,44 +1,47 @@
 import React from "react";
-import { Tabs } from "antd";
 import type { TabsProps } from "antd";
 
 import papersCountPerYear from "../../../../public/paper-count-per-year.json";
 
 import DistributionOverTime from "./visualizations/distributionOverTime/DistributionOverTime";
+import { AnalyticsContainer, AnalyticsTabs } from "./Analytics.styles";
 
 export type ColorMap = Record<string, string>;
 const data = papersCountPerYear;
+
+const visualizationsAll: TabsProps["items"] = [
+  {
+    key: "line-chart",
+    label: "Distribution of Papers",
+    forceRender: true,
+    destroyInactiveTabPane: true,
+    children: <DistributionOverTime data={data} />,
+  },
+  {
+    key: "2",
+    label: "Tab 2",
+    children: "Content of Tab Pane 2",
+  },
+  {
+    key: "3",
+    label: "Tab 3",
+    children: "Content of Tab Pane 3",
+  },
+];
+const visualizationsSelected: TabsProps["items"] = [];
 
 export const Analytics: React.FC = () => {
   const onChange = (key: string) => {
     console.log(key);
   };
-  const visualizationsAll: TabsProps["items"] = [
-    {
-      key: "line-chart",
-      label: "Distribution of Papers",
-      children: <DistributionOverTime data={data} />,
-    },
-    {
-      key: "2",
-      label: "Tab 2",
-      children: "Content of Tab Pane 2",
-    },
-    {
-      key: "3",
-      label: "Tab 3",
-      children: "Content of Tab Pane 3",
-    },
-  ];
-  const visualizationsSelected: TabsProps["items"] = [];
 
   return (
-    <>
-      <Tabs
+    <AnalyticsContainer>
+      <AnalyticsTabs
         defaultActiveKey="line-chart"
         items={visualizationsAll}
         onChange={onChange}
       />
-    </>
+    </AnalyticsContainer>
   );
 };
