3074e713c1dfe62b2ed33f2fb39bcbc369d95709
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-17 23:50:58 +0100

    Create Filters.styles.ts

---
+++ src/features/filters/Filters.styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Spacing } from "../../themes";
+
+export const FilterContentContainer = styled.div`
+    padding: ${Spacing.large} 0;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: stretch;
+`
+
+export const InputNumberFilterContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`
+export const SelectFilterContainer = styled.div`
+`
+
+export const LabelFilterContainer = styled.label`
+    text-align: start;
+    margin-bottom: ${Spacing.medium};
+    display: flex;
+    justify-content: space-between;
+    font-size: medium;
+    font-weight: 600;
+`
