04678fc7a102d59d54f2e2a2e8530958b5231175
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-17 23:50:46 +0100

    Create DistributionOverTime.tsx

---
+++ src/features/search/analytics/visualizations/distributionOverTime/DistributionOverTime.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import BubbleLineChart, {
+  BubbleLineChartProps,
+} from "../../../../../components/visulaizations/charts/BubbleLineChart";
+
+const DistributionOverTime: React.FC<BubbleLineChartProps> = ({ data }) => {
+  return <BubbleLineChart data={data} />;
+};
+
+export default DistributionOverTime;
