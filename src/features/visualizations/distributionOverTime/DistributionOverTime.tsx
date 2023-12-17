358f0497231c566c9757d48be186d9cba4bae37a
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-17 23:12:35 +0100

    Update DistributionOverTime.tsx

--- src/features/visualizations/distributionOverTime/DistributionOverTime.tsx
+++ src/features/visualizations/distributionOverTime/DistributionOverTime.tsx
@@ -3,10 +3,11 @@ import BubbleLineChart, {
   BubbleLineChartProps,
 } from "../../../components/visulaizations/charts/BubbleLineChart";
 
-const DistributionOverTime: React.FC<
-  Omit<BubbleLineChartProps, "dimensions">
-> = ({ data }) => {
-  return <BubbleLineChart data={data} />;
+const DistributionOverTime: React.FC<BubbleLineChartProps> = ({
+  data,
+  isExpanded,
+}) => {
+  return <BubbleLineChart data={data} isExpanded={isExpanded} />;
 };
 
 export default DistributionOverTime;
