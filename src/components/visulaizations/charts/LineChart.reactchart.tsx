319077e9621f4e5b8dd2571f49d90d328aad8e7c
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-22 15:21:45 +0100

    Create LineChart.reactchart.tsx

---
+++ src/components/visulaizations/charts/LineChart.reactchart.tsx
@@ -0,0 +1,71 @@
+import {
+  LineChart,
+  DotProps,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Label,
+} from "recharts";
+import papersCountPerYear from "../../../../public/paper-count-per-year.json";
+import * as d3 from "d3";
+
+const max = d3.max(papersCountPerYear, (d) => d.count) || 1;
+
+const CustomizedToolTipContent = ({ active, payload, label }: any) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`Year: ${label}`}</p>
+        <p className="label">{`Count: ${payload[0].value}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const Dot = (props: DotProps) => {
+  console.log("props", props);
+
+  return <></>;
+};
+export default function LineChartReChart() {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        data={papersCountPerYear}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 15,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="year">
+          <Label value="Years" position="bottom" />
+        </XAxis>
+        <YAxis>
+          <Label
+            value="Papers"
+            angle={-90}
+            textAnchor="middle"
+            position="insideLeft"
+          />
+        </YAxis>
+        {/* <Tooltip content={<CustomizedToolTipContent />} /> */}
+        <Tooltip />
+        <Line
+          type="natural"
+          dataKey="count"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+          dot={Dot}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+}
