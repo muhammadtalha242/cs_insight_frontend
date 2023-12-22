b5a38023d3e48e5289edf824d162521b2b0d1669
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-22 15:21:58 +0100

    Update DistributionOverTime.tsx

--- src/features/search/analytics/visualizations/distributionOverTime/DistributionOverTime.tsx
+++ src/features/search/analytics/visualizations/distributionOverTime/DistributionOverTime.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import BubbleLineChart, {
-  BubbleLineChartProps,
-} from "../../../../../components/visulaizations/charts/BubbleLineChart";
+import React, { useState } from "react";
 
-const DistributionOverTime: React.FC<BubbleLineChartProps> = ({ data }) => {
-  return <BubbleLineChart data={data} />;
+import VisualizationsContainer from "../VisualizationsContainer";
+import LineChart from "../../../../../components/visulaizations/charts/LineChart.reactchart";
+import BarChartReChart from "../../../../../components/visulaizations/charts/BarChart.reactchart";
+import { Select } from "antd";
+
+const VISUALIZATIONS_OPTIONS = [
+  { label: "Line Chart", value: "line-chart" },
+  { label: "Bar Chart", value: "bar-chart" },
+];
+
+const DistributionOverTime: React.FC = () => {
+  const [currentVisualization, setCurrentVisualization] =
+    useState("line-chart");
+  const onSelectHandle = (value: string) => {
+    setCurrentVisualization(value);
+  };
+  return (
+    <VisualizationsContainer>
+      <Select
+        options={VISUALIZATIONS_OPTIONS}
+        onChange={onSelectHandle}
+        defaultValue={currentVisualization}
+      />
+      {currentVisualization === "line-chart" ? (
+        <LineChart />
+      ) : (
+        <BarChartReChart />
+      )}
+    </VisualizationsContainer>
+  );
 };
 
 export default DistributionOverTime;
