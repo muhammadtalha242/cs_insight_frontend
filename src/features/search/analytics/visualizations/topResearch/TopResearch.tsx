eb71b41eb6145d4bdd46bfdf307764ec82d50258
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-24 04:23:48 +0100

    Create TopResearch.tsx

---
+++ src/features/search/analytics/visualizations/topResearch/TopResearch.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import VerticalBarChart from '../../../../../components/visulaizations/charts/BarChart';
+import VisualizationsContainer from '../VisualizationsContainer';
+
+const TopResearch: React.FC = () => {
+  return (
+    <VisualizationsContainer>
+      <VerticalBarChart />
+    </VisualizationsContainer>
+  );
+};
+
+export default TopResearch;
