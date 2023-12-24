2f7a738ebdfd215c4ce58e498958845337dc6ee2
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-24 03:00:35 +0100

    Update DistributionOverTime.tsx

--- src/features/search/analytics/visualizations/distributionOverTime/DistributionOverTime.tsx
+++ src/features/search/analytics/visualizations/distributionOverTime/DistributionOverTime.tsx
@@ -4,18 +4,25 @@ import { Select } from 'antd';
 
 import VerticalBarChart from '../../../../../components/visulaizations/charts/BarChart';
 import LineChart from '../../../../../components/visulaizations/charts/LineChart';
+import {
+  CHART_TITLE,
+  chart
+} from '../../../../../constants/visualizations/visualizations.types';
 import VisualizationsContainer from '../VisualizationsContainer';
 
 const VISUALIZATIONS_OPTIONS = [
-  { label: 'Line Chart', value: 'line-chart' },
-  { label: 'Bar Chart', value: 'bar-chart' }
+  { label: CHART_TITLE.line, value: chart.LINE },
+  { label: CHART_TITLE.bar, value: chart.BAR }
 ];
 
 const DistributionOverTime: React.FC = () => {
-  const [currentVisualization, setCurrentVisualization] =
-    useState('line-chart');
+  const [currentVisualization, setCurrentVisualization] = useState(
+    chart.LINE as string
+  );
 
   const onSelectHandle = (value: string) => {
+    console.log('value', value);
+
     setCurrentVisualization(value);
   };
 
@@ -35,7 +42,7 @@ const DistributionOverTime: React.FC = () => {
           defaultValue={VISUALIZATIONS_OPTIONS[0].value}
         />
       </div>
-      {currentVisualization === 'line-chart' ? (
+      {currentVisualization === chart.LINE ? (
         <LineChart />
       ) : (
         <VerticalBarChart />
