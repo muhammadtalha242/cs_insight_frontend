bd25deae7f5d445050e84a7dc10fb287e30f3c1a
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-24 20:21:36 +0100

    Update Analytics.tsx

--- src/features/search/analytics/Analytics.tsx
+++ src/features/search/analytics/Analytics.tsx
@@ -1,62 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 
-import type { TabsProps } from 'antd';
-
-import {
-  AUTHORS,
-  Dataset,
-  PAPERS,
-  VENUES
-} from '../../../constants/dataset.types';
-import {
-  Tab,
-  VISUALIZATION_TITLE,
-  visualization
-} from '../../../constants/visualizations/visualizations.types';
 import { ApplicationContext } from '../../../context/Application.context';
 
 import { AnalyticsContainer, AnalyticsTabs } from './Analytics.styles';
-import DistributionOverTime from './visualizations/distributionOverTime/DistributionOverTime';
-import TopResearch from './visualizations/topResearch/TopResearch';
-
-export const VISUALIZATION_TAB: Record<visualization, Tab> = {
-  [visualization.DISTRIBUTIONS_OVERTIME]: {
-    key: visualization.DISTRIBUTIONS_OVERTIME,
-    label: VISUALIZATION_TITLE.distribution_overtime,
-    forceRender: true,
-    destroyInactiveTabPane: true,
-    children: <DistributionOverTime />
-  },
-  [visualization.TOP_RESEARCH]: {
-    key: visualization.TOP_RESEARCH,
-    label: VISUALIZATION_TITLE.top_research,
-    forceRender: true,
-    destroyInactiveTabPane: true,
-    children: <TopResearch />
-  }
-};
-
-export const VISUALIZATION_BY_DATASET: Record<Dataset, Tab[]> = {
-  [PAPERS]: [
-    VISUALIZATION_TAB[visualization.DISTRIBUTIONS_OVERTIME],
-    VISUALIZATION_TAB[visualization.TOP_RESEARCH]
-  ],
-  [VENUES]: [VISUALIZATION_TAB[visualization.DISTRIBUTIONS_OVERTIME]],
-  [AUTHORS]: [VISUALIZATION_TAB[visualization.DISTRIBUTIONS_OVERTIME]]
-};
 
 export const Analytics: React.FC = () => {
-  const { state: applicationState } = useContext(ApplicationContext);
-
-  const [visualizationsDisplayed, setVisualizationsDisplayed] = useState<
-    TabsProps['items']
-  >([]);
-
-  useEffect(() => {
-    setVisualizationsDisplayed(
-      VISUALIZATION_BY_DATASET[applicationState.dataSet]
-    );
-  }, [applicationState.dataSet]);
+  const {
+    state: { currentVisualizations }
+  } = useContext(ApplicationContext);
 
   const onChange = (key: string) => {
     console.log(key);
@@ -64,7 +15,7 @@ export const Analytics: React.FC = () => {
 
   return (
     <AnalyticsContainer>
-      <AnalyticsTabs items={visualizationsDisplayed} onChange={onChange} />
+      <AnalyticsTabs items={currentVisualizations} onChange={onChange} />
     </AnalyticsContainer>
   );
 };
