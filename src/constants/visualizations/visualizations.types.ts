f8a4d54dfc5b599f06f8578c2e0904171166ff00
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-24 03:00:24 +0100

    Update visualizations.types.ts

--- src/constants/visualizations/visualizations.types.ts
+++ src/constants/visualizations/visualizations.types.ts
@@ -1,3 +1,5 @@
+import { AUTHORS, Dataset, PAPERS, VENUES } from '../dataset.types';
+
 export enum visualizations {
   DISTRIBUTIONS_OVERTIME = 'distribution_overtime'
 }
@@ -9,3 +11,21 @@ export enum chart {
   MULTI_LINE = 'multi_line',
   BAR = 'bar'
 }
+
+export const VISUALIZATION_TITLE: Record<visualizations, string> = {
+  [visualizations.DISTRIBUTIONS_OVERTIME]: 'Distribution Over Time'
+} as const;
+
+export const CHART_TITLE: Record<chart, string> = {
+  [chart.BAR]: 'Bar Chart',
+  [chart.AREA]: 'Area Chart',
+  [chart.AREA_STACKED]: 'Stacked Area Chart',
+  [chart.LINE]: 'Line Chart',
+  [chart.MULTI_LINE]: 'Multi Line Chart'
+} as const;
+
+export const VISUALIZATION_BY_DATASET: Record<Dataset, visualizations[]> = {
+  [PAPERS]: [visualizations.DISTRIBUTIONS_OVERTIME],
+  [AUTHORS]: [visualizations.DISTRIBUTIONS_OVERTIME],
+  [VENUES]: [visualizations.DISTRIBUTIONS_OVERTIME]
+} as const;
