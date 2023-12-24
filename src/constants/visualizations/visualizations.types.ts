662a3b8ca78fea97c38c4472143eb8176f9033d3
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-24 01:35:23 +0100

    Create visualizations.types.ts

---
+++ src/constants/visualizations/visualizations.types.ts
@@ -0,0 +1,11 @@
+export enum visualizations {
+  DISTRIBUTIONS_OVERTIME = 'distribution_overtime'
+}
+
+export enum chart {
+  AREA = 'area',
+  AREA_STACKED = 'area_stacked',
+  LINE = 'line',
+  MULTI_LINE = 'multi_line',
+  BAR = 'bar'
+}
