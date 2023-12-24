cf167ff8c78c2c3bcf75c9e5c8b42634c046ad41
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-24 03:00:37 +0100

    Update Visualizations.styles.ts

--- src/features/search/analytics/visualizations/Visualizations.styles.ts
+++ src/features/search/analytics/visualizations/Visualizations.styles.ts
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
 
-export const VisualizationContainer = styled.div``;
+export const VisualizationContainer = styled.div<{ height: number }>`
+  width: 100%;
+  height: ${props => props.height}px;
+`;
