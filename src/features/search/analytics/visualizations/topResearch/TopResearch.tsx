61b7c99429449bff7d54555024ba6f185416c7da
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-12-28 16:26:28 +0100

    Update TopResearch.tsx

--- src/features/search/analytics/visualizations/topResearch/TopResearch.tsx
+++ src/features/search/analytics/visualizations/topResearch/TopResearch.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 
 import VerticalBarChart from '../../../../../components/visulaizations/charts/BarChart';
+import visualizationsService from '../../../../../services/visualizations';
 import VisualizationsContainer from '../VisualizationsContainer';
 
 const TopResearch: React.FC = () => {
+  const [data, setData] = useState<{ count: number; year: number }[]>([]);
+
+  useLayoutEffect(() => {
+    const getData = async () => {
+      const graphData: { count: number; year: number }[] =
+        await visualizationsService.getPapersCount();
+
+      //TODO: useMemo
+      const dt = () =>
+        graphData.map(v => ({ count: Number(v.count), year: v.year }));
+
+      setData(dt);
+    };
+
+    getData();
+  }, []);
+
   return (
     <VisualizationsContainer>
-      <VerticalBarChart />
+      {data.length !== 0 ? <VerticalBarChart data={data} /> : null}
     </VisualizationsContainer>
   );
 };
