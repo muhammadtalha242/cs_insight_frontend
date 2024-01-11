1a62b54b7da803ff27ed523188414a3600200185
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-01-11 23:04:34 +0100

    Create autocomplete.ts

---
+++ src/services/autocomplete.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import { AUTOCOMPLETE_ROUTES, API_BASE_URL } from '../constants/consts';
+
+type autocompleteProps = { route: AUTOCOMPLETE_ROUTES; inputValue: string };
+
+const autocomplete = async ({ route, inputValue }: autocompleteProps) => {
+  if (!inputValue) return [];
+
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/search/${route}?q=${inputValue}`
+    );
+
+    return response.data;
+  } catch (error) {
+    throw new Error('Network response error');
+  }
+};
+
+export { autocomplete };
