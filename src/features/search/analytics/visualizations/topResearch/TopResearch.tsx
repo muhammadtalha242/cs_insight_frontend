b49e2e5354c77abc74896e0a949ed130334c0d52
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2024-01-11 23:04:31 +0100

    Update TopResearch.tsx

--- src/features/search/analytics/visualizations/topResearch/TopResearch.tsx
+++ src/features/search/analytics/visualizations/topResearch/TopResearch.tsx
@@ -1,30 +1,30 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useContext, useLayoutEffect, useState } from 'react';
+
+import { useQuery } from 'react-query';
 
 import VerticalBarChart from '../../../../../components/visulaizations/charts/BarChart';
+import { QueryContext } from '../../../../../context/Query.context';
 import visualizationsService from '../../../../../services/visualizations';
 import VisualizationsContainer from '../VisualizationsContainer';
 
 const TopResearch: React.FC = () => {
-  const [data, setData] = useState<{ count: number; year: number }[]>([]);
+  const { state } = useContext(QueryContext);
+  const { data, isLoading, error } = useQuery(
+    ['myData', state.filters], // Query key is now an array
+    visualizationsService.getPapersCount
+  );
+  let graphData: { count: number; year: number }[] = [];
 
-  useLayoutEffect(() => {
-    const getData = async () => {
-      const graphData: { count: number; year: number }[] =
-        await visualizationsService.getPapersCount();
-
-      //TODO: useMemo
-      const dt = () =>
-        graphData.map(v => ({ count: Number(v.count), year: v.year }));
-
-      setData(dt);
-    };
-
-    getData();
-  }, []);
+  if (!isLoading) {
+    graphData = data.map((v: { count: number; year: number }) => ({
+      count: Number(v.count),
+      year: v.year
+    }));
+  }
 
   return (
-    <VisualizationsContainer>
-      {data.length !== 0 ? <VerticalBarChart data={data} /> : null}
+    <VisualizationsContainer isLoading={isLoading} error={error}>
+      {graphData.length !== 0 ? <VerticalBarChart data={graphData} /> : null}
     </VisualizationsContainer>
   );
 };
