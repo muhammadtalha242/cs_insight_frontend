6fdbd422bda22fd9b6578ca30fbdfb4f2ed397f1
Muhammad Talha <m.talha2244@gmail.com>
2024-03-26 02:05:44 +0500

    Update visualizations.ts

--- src/services/visualizations.ts
+++ src/services/visualizations.ts
@@ -1,17 +1,27 @@
 import axios from 'axios';
 
 import { API_BASE_URL } from '../constants/consts';
+import { Filter } from '../constants/types';
 import { getUrlSearchParams } from '../util/urlSearchParams';
 
 const getPapersCount = async ({ queryKey }: any) => {
   const [, filters] = queryKey;
+
+  console.log('queryKey', queryKey);
   const queryParams = getUrlSearchParams(filters);
 
   const res = await axios.get(`${API_BASE_URL}/papers/years?${queryParams}`);
 
   return res.data;
 };
+const getPapersCountPost = async (queryParams: Filter) => {
+  console.log('queryParams', queryParams);
+  const res = await axios.post(`${API_BASE_URL}/papers/years`, queryParams);
+
+  return res.data;
+};
 
 export default {
-  getPapersCount
+  getPapersCount,
+  getPapersCountPost
 };
