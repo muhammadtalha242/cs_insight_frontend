117fbf82e51057568ac52964c6b182f428ee3025
Muhammad Talha <m.talha2244@gmail.com>
2024-03-26 02:05:40 +0500

    Update autocomplete.ts

--- src/services/autocomplete.ts
+++ src/services/autocomplete.ts
@@ -1,21 +1,31 @@
 import axios from 'axios';
+import { useQuery } from 'react-query';
 
 import { AUTOCOMPLETE_ROUTES, API_BASE_URL } from '../constants/consts';
 
 type autocompleteProps = { route: AUTOCOMPLETE_ROUTES; inputValue: string };
 
-const autocomplete = async ({ route, inputValue }: autocompleteProps) => {
-  if (!inputValue) return [];
+export const useAutocomplete = ({ route, inputValue }: autocompleteProps) => {
+  const autocomplete = async () => {
+    if (!inputValue) return [];
 
-  try {
-    const response = await axios.get(
-      `${API_BASE_URL}/autocomplete/${route}?q=${inputValue}`
-    );
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/autocomplete/${route}?q=${inputValue}`
+      );
 
-    return response.data;
-  } catch (error) {
-    throw new Error('Network response error');
-  }
+      return response.data.map((item: { id: string; value: string }) => ({
+        key: item.id,
+        value: item.value
+      }));
+    } catch (error) {
+      throw new Error('Network response error');
+    }
+  };
+
+  const { data, isFetching } = useQuery(['routes', inputValue], autocomplete, {
+    enabled: inputValue.length > 1
+  });
+
+  return { data, isFetching };
 };
-
-export { autocomplete };
